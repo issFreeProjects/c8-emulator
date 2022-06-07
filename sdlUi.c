fca9fbc50caf28655feb2feb9187d7631c902e10
iss <iss.siamak@gmail.com>
2022-06-07 11:04:50 +0430

    cleaning and fixing some key handeling issues

--- sdlUi.c
+++ sdlUi.c
@@ -4,10 +4,8 @@
 void handel_events()
 {
     SDL_PollEvent (&e);
-    switch(e.type){
-        case SDL_QUIT:
-            quit(0);
-    }
+    if(keys[SDLK_ESCAPE] || keys[SDLK_q] || e.type==SDL_QUIT)
+        quit(0);
     if(keys[SDLK_p]){
         while(1){
             SDL_WaitEvent(&e);
@@ -31,17 +29,19 @@ void init_ui()
 }
 
 
-void draw_screen(unsigned char* screen, int W, int H)
+void draw_screen(unsigned char* screen, int pix_W, int pix_H)
 {
     int x, y, i, j;
     SDL_Surface *surface = SDL_GetVideoSurface();
     SDL_LockSurface(surface);
     Uint32 *pixels = (Uint32 *)surface->pixels;
+
     for (x=0; x<WINDOW_W; x++)
-        for(y=0; y<WINDOW_H; y++){
-            i = x/(WINDOW_W/W);
-            j = y/(WINDOW_H/H);
-            pixels[x+y*WINDOW_W] = screen[i+j*W] ? WHITE_COL: BLACK_COL;
+        for(y=0; y<WINDOW_H; y++)
+        {
+            i = x/(WINDOW_W/pix_W);
+            j = y/(WINDOW_H/pix_H);
+            pixels[x+y*WINDOW_W] = screen[i+j*pix_W] ? WHITE_COL: BLACK_COL;
         }
     SDL_UnlockSurface(surface);
     SDL_Flip(surface);
